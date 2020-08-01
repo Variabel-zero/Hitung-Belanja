99177ffd02796c57a9bbc9b5a7ec5157ad3aa970
VarZero <58689926+Variabel-zero@users.noreply.github.com>
2020-08-02 00:54:44 +0700

    hitung belanja
    mengupload filemeng hitung belanja.py

---
+++ Hitung Belanja.py
@@ -0,0 +1,9 @@
+print("Toko baju Baru")
+Nomor1 = str(input("Masukkan nama baju :"))
+Nomor2 = int(input("Masukkan harga baju :"))
+Nomor3 = int(input("Jumlah BAJU YANG DIBELI : "))
+Nomor4 = int(input("Jumlah uang yang dibayar : "))
+
+hitung = (Nomor2*Nomor3)
+total = (Nomor4-hitung)
+print("total kembalian : ","Rp.",total)
